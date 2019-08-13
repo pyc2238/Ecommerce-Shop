6ad08923ec9374419f5e4ec63db28e015a4f2e3c
パク・ボグン <33335769+pyc2238@users.noreply.github.com>
2019-08-13 10:22:08 +0900

    Design Admin page

---
+++ resources/views/admin/nav.blade.php
@@ -0,0 +1,33 @@
+
+  
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+        <div class="sidebar-sticky">
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+                <span data-feather="home"></span>
+                Dashboard <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file"></span>
+                Add Products
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="shopping-cart"></span>
+                List Products
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="users"></span>
+                Categories
+              </a>
+            </li>
+           
+          </ul>
+        </div>
+      </nav>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('front.home');
 });
+
+Route::get('admin', function () {
+    return view('admin.index');
+});
